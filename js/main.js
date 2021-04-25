aa63cb0ee79c311c84f26868caefc36ffbaad7a3
Finn Carver <77501622+orangehoneybee@users.noreply.github.com>
2021-04-24 21:46:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "../portfolios/week1/index.html"  
+    } 
+  ]
+
+var olist = document.getElementById("list");
+
+for (let i=0; i < links.length; i++){
+    let a = document.createElement("a");
+    let li = document.createElement("li");
+    a.textContent = links[i].label;
+    a.setAttribute('href', links[i].url);
+    li.appendChild(a);
+    olist.appendChild(li); 
+}
 
